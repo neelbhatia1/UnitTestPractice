e3d24b76a6f62359a7902cfecd751b45c10b53c0
neelbhatia1 <neelb384@gmail.com>
2024-04-15 12:33:33 -0700

    Adds the has_mixed_case function and fixes bugs in count leading characters.

--- Password.cpp
+++ Password.cpp
@@ -10,6 +10,9 @@ using std::string;
   'z' and any ASCII characters are allowed.
 */
 int Password::count_leading_characters(string phrase){
+  if (phrase.empty())
+    return 0; // Return 0 for an empty string
+
   int repetition = 1;
   int index = 0;
   while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
@@ -17,4 +20,25 @@ int Password::count_leading_characters(string phrase){
     index++;
   }
   return repetition;
+}
+
+bool Password::has_mixed_case(string word) {
+  if (word.empty()) {
+    return false; // Return false for an empty string
+  }
+
+  bool has_lower = false;
+  bool has_upper = false;
+
+  for (char c : word) {
+    if (islower(c)) {
+      has_lower = true;
+    } else if (isupper(c)) {
+      has_upper = true;
+    }
+    // Break the loop if both cases are found
+    if (has_lower && has_upper) break;
+  }
+  // Return true only if both lower and upper cases are found
+  return (has_lower && has_upper);
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,82 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, same_leading_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaaaaa");
+	ASSERT_EQ(6, actual);
+}
+
+TEST(PasswordTest, mixed_leading_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaazzz");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, mixed_case_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zZzZzZz");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, mixed_case_and_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("abcABCfds");
+	ASSERT_EQ(1, actual);
+} 
+
+TEST(PasswordTest, mixed_case_and_num_and_sym_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("##abcABCfds");
+	ASSERT_EQ(2, actual);
+}
+
+//-----------------------------------------------------------------------
+
+TEST(PasswordTest, has_mixed_case_both_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("PassWord");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PasswordTest, has_mixed_case_upper_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("PASSWORD");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, has_mixed_case_lower_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("password");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, has_mixed_case_empty_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, has_mixed_case_numbers_and_symbols_password)
+{
+	Password my_password;
+	bool actual = my_password.has_mixed_case("pass123*#%");
+	ASSERT_FALSE(actual);
+}
